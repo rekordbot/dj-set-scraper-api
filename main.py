0650ef4c68bae2ade49b8b37b1d73d282f28ffa5
rekordbot <bugbear.thunk0@icloud.com>
2025-05-24 14:07:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from scrape_trackid import scrape_trackid_mixes
+import logging
 
 app = FastAPI()
 
 @app.get("/trackid")
 def trackid(artist: str):
-    return scrape_trackid_mixes(artist)
+    print(f"Received request for artist: {artist}")
+    try:
+        return scrape_trackid_mixes(artist)
+    except Exception as e:
+        logging.exception("Scraper failed")
+        return {"error": str(e)}
